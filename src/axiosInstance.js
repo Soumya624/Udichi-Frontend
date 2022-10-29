ce74b3559fa23a41f5a626bc77b1bbfd2956abd0
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2022-10-29 16:47:15 +0530

    Added

---
+++ src/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+console.log(process.env.REACT_APP_BASE_URL)
+
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
+})
+
+export default axiosInstance
