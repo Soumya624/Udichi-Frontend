6a7558a02baef9ead5b2b444af76ceaa2859eefd
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-11-25 11:53:59 +0530

    Added

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
+    baseURL: process.env.REACT_APP_BASE_URL || "https://udichi-server-testing.onrender.com",
 })
 
 export default axiosInstance
