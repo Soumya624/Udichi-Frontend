3927ebd4a8b15241852ecaf562ddab236da2c57d
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2023-01-14 13:27:57 +0530

    Changes in backend link...

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || "https://udichi-server-testing.onrender.com",
+    baseURL: process.env.REACT_APP_BASE_URL || "https://udichiservertesting-production.up.railway.app/",
     // baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
 })
 
