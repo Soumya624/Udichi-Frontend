3360ada9167bd88ed0884998a273210029e1f870
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2023-03-13 01:41:37 +0530

    Added

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    //baseURL: process.env.REACT_APP_BASE_URL || "https://udichiservertesting-production.up.railway.app/",
-    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
+    baseURL: process.env.REACT_APP_BASE_URL || "https://udichiserver-production.up.railway.app/",
+    //baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
 })
 
 export default axiosInstance
